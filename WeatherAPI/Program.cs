be44953f91c823c31364820c9e25d23e82024ee4
Yun Jang <jang.yunj7@gmail.com>
2023-02-06 22:48:05 -0500

    corrected fahrenheit

--- WeatherAPI/Program.cs
+++ WeatherAPI/Program.cs
@@ -19,9 +19,15 @@ var client = new HttpClient();
 Console.WriteLine("What is your zip code? ");
 var zipCode = Console.ReadLine();
 var APIkey = "9c8e7b682ff697f406da2117299f305c";
-var server = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIkey}";
+var server1 = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIkey}&units=imperial";
+var server2 = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIkey}&units=metric";
 
-var serverResponse = client.GetStringAsync(server).GetAwaiter().GetResult();
-var serverObject = JObject.Parse(serverResponse);
+var serverResponse1 = client.GetStringAsync(server1).Result;
+var serverResponse2 = client.GetStringAsync(server2).Result;
+var serverObject1 = JObject.Parse(serverResponse1);
+var serverObject2 = JObject.Parse(serverResponse2);
 
-Console.WriteLine($"Current weather for {zipCode} is: {serverObject["main"]["temp"]}");
+Console.WriteLine($"Current weather for {zipCode} is: {serverObject1["main"]["temp"]} Fahrenheit");
+Console.WriteLine($"Current weather for {zipCode} is: {serverObject2["main"]["temp"]} Celcius");
+
+
