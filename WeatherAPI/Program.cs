6897833f6dc65681dc75e71468177cb1a8661af7
Yun Jang <jang.yunj7@gmail.com>
2023-02-06 21:29:08 -0500

    initial commit

---
+++ WeatherAPI/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+
+var client = new HttpClient();
+
+var APIkey = "9c8e7b682ff697f406da2117299f305c";
+Console.Write("What city are you in? ");
+var cityName = Console.ReadLine();
+
+var server = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={APIkey}&units=imperial";
+
+var serverResponse = client.GetStringAsync(server).GetAwaiter().GetResult();
+var serverObject = JObject.Parse(serverResponse);
+
+Console.WriteLine($"The current weather at {cityName} is: {serverObject["main"]["temp"]}");
+
+
