a91a5c4513b2f31c1f3aaaf8587c59a78e4f963b
Yun Jang <jang.yunj7@gmail.com>
2023-02-06 21:53:25 -0500

    added zip code server

--- WeatherAPI/Program.cs
+++ WeatherAPI/Program.cs
@@ -1,16 +1,29 @@
 ﻿using Newtonsoft.Json.Linq;
 
+//var client = new HttpClient();
+
+//var APIkey = "9c8e7b682ff697f406da2117299f305c";
+//Console.Write("What city are you in? ");
+//var cityName = Console.ReadLine();
+
+//var server = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={APIkey}&units=imperial";
+
+//var serverResponse = client.GetStringAsync(server).GetAwaiter().GetResult();
+//var serverObject = JObject.Parse(serverResponse);
+
+//Console.WriteLine($"The current weather at {cityName} is: {serverObject["main"]["temp"]}");
+
+
 var client = new HttpClient();
 
 var APIkey = "9c8e7b682ff697f406da2117299f305c";
-Console.Write("What city are you in? ");
-var cityName = Console.ReadLine();
 
-var server = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={APIkey}&units=imperial";
+Console.WriteLine("Enter your zip code: ");
+var zipCode = Console.ReadLine();
 
-var serverResponse = client.GetStringAsync(server).GetAwaiter().GetResult();
+var server = "https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIkey}";
+
+var serverResponse = client.GetStringAsync(server).Result;
 var serverObject = JObject.Parse(serverResponse);
 
-Console.WriteLine($"The current weather at {cityName} is: {serverObject["main"]["temp"]}");
-
-
+Console.WriteLine($"Current weather for {zipCode} is: {serverObject["main"]["temp"]}");
